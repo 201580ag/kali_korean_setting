1896f7034124ca759fc09661a6560ef47690d170
201580ag <39054577+201580ag@users.noreply.github.com>
2023-08-06 21:49:32 +0900

    Update Korean_patch.py

--- Korean_patch.py
+++ Korean_patch.py
@@ -4,14 +4,14 @@ import os
 
 # Check if the script is running with root privileges
 if os.geteuid() != 0:
-    print("This script must be executed with administrative privileges. Please run with sudo.")
+    print("\033[31mThis script must be executed with administrative privileges. Please run with sudo.\033[0m")
     exit(1)
 
-print("Developer by 201580ag")
-print("Starting the installation script...")
+print("\033[33mDeveloper by 201580ag\033[0m")
+print("\033[32mStarting the installation script...\033[0m")
 
 # Prompt for Korean font patch installation
-choice = input("Do you want to proceed with the Korean font patch installation? (y/n): ")
+choice = input("\033[31mDo you want to proceed with the Korean font patch installation? (y/n): \033[0m")
 if choice.lower() == 'y':
     # Commands to install Korean fonts and input method
     os.system('apt-get update && apt-get upgrade')
@@ -21,12 +21,12 @@ if choice.lower() == 'y':
     print("Korean font patch installation completed.")
 
     # Prompt for system restart
-    restart_choice = input("Do you want to restart your system now? (y/n): ")
+    restart_choice = input("\033[31mDo you want to restart your system now? (y/n): \033[0m")
     if restart_choice.lower() == 'y':
         os.system('reboot')
     else:
-        print("Please make sure to restart your system manually for changes to take effect.")
+        print("\033[31mPlease make sure to restart your system manually for changes to take effect.\033[0m")
 elif choice.lower() == 'n':
-    print("Korean font patch installation cancelled.")
+    print("\033[31mKorean font patch installation cancelled.\033[0m")
 else:
-    print("Invalid input. Please enter 'y' or 'n' only.")
+    print("\033[31mInvalid input. Please enter 'y' or 'n' only.\033[0m")
